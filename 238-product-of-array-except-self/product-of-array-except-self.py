36e4e75d2ace262fe7d30a4c92f4113702576645
Mohammed Mudassir Faaiz <23A91A1239@aec.edu.in>
2026-05-20 11:36:09 +0530

    Time: 17 ms (82.96%) | Memory: 25.1 MB (89.64%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,18 @@
+class Solution:
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        answer = [1] * n
+
+        # Calculate prefix products
+        prefix = 1
+        for i in range(n):
+            answer[i] = prefix
+            prefix *= nums[i]
+
+        # Calculate suffix products and multiply
+        suffix = 1
+        for i in range(n - 1, -1, -1):
+            answer[i] *= suffix
+            suffix *= nums[i]
+
+        return answer
