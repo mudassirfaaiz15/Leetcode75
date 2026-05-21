31ccf23eb6b77009779d7ccba3e7154e6db2250f
Mohammed Mudassir Faaiz <23A91A1239@aec.edu.in>
2026-05-21 18:17:07 +0530

    Time: 1 ms (26.95%) | Memory: 22.2 MB (18.27%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def deleteNode(self, root, key):
+
+        if not root:
+            return None
+
+        # Search for the node
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+
+        else:
+            # Node found
+
+            # Case 1: No left child
+            if not root.left:
+                return root.right
+
+            # Case 2: No right child
+            if not root.right:
+                return root.left
+
+            # Case 3: Both children exist
+            # Find inorder successor (smallest in right subtree)
+            temp = root.right
+
+            while temp.left:
+                temp = temp.left
+
+            root.val = temp.val
+
+            # Delete inorder successor
+            root.right = self.deleteNode(root.right, temp.val)
+
+        return root
