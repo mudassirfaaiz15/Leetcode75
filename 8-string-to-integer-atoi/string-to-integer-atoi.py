1bd21dcb2f10c636a2995408577fdc4ddf482a08
Mohammed Mudassir Faaiz <23A91A1239@aec.edu.in>
2026-05-23 21:54:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (13.30%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,36 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+
+        i = 0
+        n = len(s)
+
+        # Remove leading spaces
+        while i < n and s[i] == ' ':
+            i += 1
+
+        # Check sign
+        sign = 1
+        if i < n and (s[i] == '+' or s[i] == '-'):
+            if s[i] == '-':
+                sign = -1
+            i += 1
+
+        # Convert number
+        num = 0
+        while i < n and s[i].isdigit():
+            num = num * 10 + int(s[i])
+            i += 1
+
+        num *= sign
+
+        # Clamp to 32-bit signed integer range
+        INT_MIN = -2**31
+        INT_MAX = 2**31 - 1
+
+        if num < INT_MIN:
+            return INT_MIN
+
+        if num > INT_MAX:
+            return INT_MAX
+
+        return num
