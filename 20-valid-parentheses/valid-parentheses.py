03dbed321ca516d8660dc57b9c3eb2dac7660330
Mohammed Mudassir Faaiz <23A91A1239@aec.edu.in>
2026-06-03 16:59:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (66.97%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -3,21 +3,20 @@ class Solution:
         
         stack = []
 
-        brackets = {
+        pairs = {
             ')': '(',
-            ']': '[',
-            '}': '{'
+            '}': '{',
+            ']': '['
         }
 
         for ch in s:
 
-            # Opening bracket
-            if ch in "([{":
+            if ch in "({[":
                 stack.append(ch)
 
-            # Closing bracket
             else:
-                if not stack or stack[-1] != brackets[ch]:
+
+                if not stack or stack[-1] != pairs[ch]:
                     return False
 
                 stack.pop()
